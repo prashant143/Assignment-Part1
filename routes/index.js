54f2c04f152e824ccbd0b7ddd3fb845a5c2b9241
prashant143 <prashant.vaghela@mygeorgian.ca>
2016-10-05 00:52:44 -0400

    Assignment Part1 Created with NodeJs, ExpressJs, HTML and CSS

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Prahshant Vaghela' });
+});
+router.get('/education', function(req, res, next) {
+  res.render('Education', { title: 'Education - Prahshant Vaghela' });
+});
+router.get('/work', function(req, res, next) {
+  res.render('work', { title: 'Work - Prahshant Vaghela' });
+});
+router.get('/skills', function(req, res, next) {
+  res.render('skills', { title: 'Skills - Prahshant Vaghela' });
+});
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'Portfolio - Prahshant Vaghela' });
+});
+
+module.exports = router;
